9d8bbfb5ee86a7332ddd531eae96bc81e994ff20
Ghita <bahajghita3@gmail.com>
2023-04-01 23:03:17 +0000

    crud category

--- FillRouge/app/Http/Controllers/CategoryController.php
+++ FillRouge/app/Http/Controllers/CategoryController.php
@@ -2,11 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     
+     public function store(Request $request){
+        $category = Category::create($request->all());
+     }
 
+     
+    public function destroy(Category $category, $id)
+    {
+        Category::destroy($id);
+    
+    }
+     
+
+    public function show($id)
+    {
+        if(Genre::find($id)){
+           
+        }
+    
+    }
+
+    public function update(Request $request, $id)
+    {
+        $category_update = Category::find($id);
+        $Category_update->update($request->all());
+      
+    }
 
 }

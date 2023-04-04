16a48356f3dc8e0aa8908be1330685ea963275b5
Ghita <bahajghita3@gmail.com>
2023-04-04 14:55:47 +0000

    main  dashboard

--- FillRouge/resources/views/layouts/app.blade.php
+++ FillRouge/resources/views/layouts/app.blade.php
@@ -16,7 +16,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <link rel="stylesheet" href="{{asset('css/sign.css') }}" >
     <link rel="stylesheet" href="{{asset('css/dash.css') }}" >
 
@@ -29,6 +28,7 @@
             @yield('content')   
     </div>
     <script src="{{asset('js/sign.js') }}" ></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="{{asset('js/dash.js') }}"></script>
 </body>

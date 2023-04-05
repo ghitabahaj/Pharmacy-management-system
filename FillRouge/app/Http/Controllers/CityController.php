99def8728455a6d33017be776b73b3cd4882dde6
Ghita <bahajghita3@gmail.com>
2023-04-05 15:02:54 +0000

    city controller

--- FillRouge/app/Http/Controllers/CityController.php
+++ FillRouge/app/Http/Controllers/CityController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
     public function addCity(){
-
+        $city = new City();
+        $input = $request->all();
+        $city->fill($input);
+        $city->save();
+        return redirect()->route('city'); 
     }
 }

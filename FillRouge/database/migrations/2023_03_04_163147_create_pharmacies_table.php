fab4c6e079300eb3cb5d68c5e0c40738efc12bd6
Ghita <bahajghita3@gmail.com>
2023-04-05 14:55:55 +0000

    main  dashboard

--- FillRouge/database/migrations/2023_03_04_163147_create_pharmacies_table.php
+++ FillRouge/database/migrations/2023_03_04_163147_create_pharmacies_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('location');
             $table->string('telephone');
             $table->string('city');
+            $table->string('employees');
         });
     }
 
--- FillRouge/database/migrations/2023_03_30_092111_create_cities_table.php
+++ FillRouge/database/migrations/2023_03_30_092111_create_cities_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('province');
+            $table->integer('pharmaciesnum');
+            $table->integer('PostalCode');
             $table->timestamps();
         });
     }
